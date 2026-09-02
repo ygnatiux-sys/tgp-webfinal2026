6731d37c894d975e8d566241770d7aa7f3ec53bb
Xavier Benitez • The Great Puzzle Project <ygnatiux@gmail.com>
2026-09-02 19:45:41 -0300

    fix: consolidar site y resolver colision wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alternative.thegreatpuzzleproject.com',
   output: 'static',
 
   server: {
